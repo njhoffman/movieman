8131543680263bff89fa70b747778c4356977833
Nicholas Hoffman <njhoffman1982@gmail.com>
2020-04-05 16:51:06 -0500

    replace html-get with curl

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 module.exports = {
   scraper: {
-    url: 'https://thepiratebay.org/search',
-    // pageSuffix: '/0/7/200',
-    urlSuffix: '7/200',
+    url: 'https://thepiratebays.info/search',
+    urlSuffix: '99/207',
+    // urlSuffix: '7/200',
     fetchInterval: 3000,
     minSeeders: 3,
     outputPath: '.cache/files.json'
