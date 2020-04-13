602885e2dce11f09a71049f79390c7179cb31809
Nicholas Hoffman <njhoffman1982@gmail.com>
2020-04-13 15:06:39 -0500

    add writer module

--- config/index.js
+++ config/index.js
@@ -1,11 +1,20 @@
 module.exports = {
+  search: '1982',
   scraper: {
     url: 'https://thepiratebays.info/search',
+    // filters for only hd movies
     urlSuffix: '99/207',
     // urlSuffix: '7/200',
-    fetchInterval: 3000,
-    minSeeders: 3,
-    outputPath: '.cache/files.json'
+    minSeeders: 1,
+    fetchInterval: 3000
   },
-  existing: ['/mnt/e/Sorted/Movies']
+  filter: {
+    existing: ['/mnt/e/Sorted/Movies'],
+    minSeeders: 1,
+    exclude: '720p'
+  },
+  writer: {
+    outputFolder: '.torrents',
+    historyFile: 'torrents.json'
+  }
 };
